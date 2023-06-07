163995b2d4a0b4bb2e778183eef058a148ffbf21
Dome <tecnoemod@gmail.com>
2023-06-07 19:25:30 +0200

    Added Sqlite stuff

--- MovieList/Program.cs
+++ MovieList/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MovieList.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +27,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("WebApiMovieDatabase"))
+    );
 
 var app = builder.Build();
 
