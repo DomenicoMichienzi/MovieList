d4867a4ad4b70708140bd72121b95fe19a0150e4
Dome <tecnoemod@gmail.com>
2023-06-13 16:31:12 +0200

    Added Linq Dynamic Core and paging, sorting and filter on MoviesController

--- MovieList/Controllers/MoviesController.cs
+++ MovieList/Controllers/MoviesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using MovieList.DTO;
 using MovieList.Models;
+using System.Linq.Dynamic.Core;
 
 namespace MovieList.Controllers
 {
@@ -20,20 +21,39 @@ namespace MovieList.Controllers
 
         [HttpGet(Name = "GetMovies")]
         [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
-        public async Task<RestDTO<Movie[]>> Get()
+        public async Task<RestDTO<Movie[]>> Get(
+            int pageIndex = 0,
+            int pageSize = 10,
+            string? sortColumn = "Title",
+            string? sortOrder = "ASC",
+            string? filterQuery = null)
         {
-            var query = _context.Movies;
+            var query = _context.Movies.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(filterQuery))
+                query = query.Where(m => m.Title.Contains(filterQuery));
+
+            var recordCount = await query.CountAsync();
+
+            query = query
+                .OrderBy($"{sortColumn} {sortOrder}")
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize);
             
             return new RestDTO<Movie[]>()
             {
                 Data = await query.ToArrayAsync(),
-                Links = new List<LinkDTO>
-                {
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                RecordCount = recordCount,
+                Links = new List<LinkDTO> {
                     new LinkDTO(
-                        Url.Action(null, "Movies", null, Request.Scheme)!,
+                        Url.Action(
+                            null,
+                            "Movies",
+                            new { pageIndex, pageSize },
+                            Request.Scheme)!,
                         "self",
-                        "GET"
-                    )
+                        "GET"),
                 }
             };
         }
