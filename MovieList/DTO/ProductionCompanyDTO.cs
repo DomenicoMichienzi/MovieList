97b8e2e975267fb92dade3ea1be035cedc2a3286
Dome <tecnoemod@gmail.com>
2023-06-30 19:09:12 +0200

    Added ProductionCompanyDTO

---
+++ MovieList/DTO/ProductionCompanyDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieList.DTO;
+
+public class ProductionCompanyDTO
+{
+    [Required]
+    public int Id { get; set; }
+    
+    public string? Name { get; set; }
+}
