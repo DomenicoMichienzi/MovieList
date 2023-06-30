e3289225fb81fa8a59581b2817f4670bbc13cecf
Dome <tecnoemod@gmail.com>
2023-06-30 21:02:26 +0200

    Added GenresDTO

---
+++ MovieList/DTO/GenresDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieList.DTO;
+
+public class GenresDTO
+{
+    [Required]
+    public int Id { get; set; }
+    
+    public string? Name { get; set; }
+}
