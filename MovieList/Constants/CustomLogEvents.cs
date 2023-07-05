228a8c176d4bacbc340ac3d92b1c305139c098f2
Dome <tecnoemod@gmail.com>
2023-07-05 18:59:56 +0200

    Create CustomLogEvents class

---
+++ MovieList/Constants/CustomLogEvents.cs
@@ -0,0 +1,21 @@
+﻿namespace MovieList.Constants;
+
+public class CustomLogEvents
+{
+    public const int Error_Get = 5001;
+
+    public const int MoviesController_Get = 50110;
+    public const int MoviesController_Post = 50120;
+    public const int MoviesController_Put = 50130;
+    public const int MoviesController_Delete = 50140;
+
+    public const int GenresController_Get = 50210;
+    public const int GenresController_Post = 50220;
+    public const int GenresController_Put = 50230;
+    public const int GenresController_Delete = 50240;
+
+    public const int ProductionCompnies_Get = 50310;
+    public const int ProductionCompnies_Post = 50320;
+    public const int ProductionCompnies_Put = 50330;
+    public const int ProductionCompnies_Delete = 50340;
+}
