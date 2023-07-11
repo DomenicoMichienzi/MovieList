895ef0cdfaf3cdae40f68fb3519c5f0fc239aeaa
Dome <tecnoemod@gmail.com>
2023-07-11 15:58:54 +0200

    implemented Login function

--- MovieList/Controllers/AccountController.cs
+++ MovieList/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MovieList.DTO;
 using MovieList.Models;
 
@@ -82,8 +85,67 @@ public class AccountController : ControllerBase
 
     [HttpPost]
     [ResponseCache(CacheProfileName = "NoCache")]
-    public async Task<ActionResult> Login()
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<ActionResult> Login(LoginDTO input)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = await _userManager.FindByNameAsync(input.UserName);
+                    if (user == null
+                        || !await _userManager.CheckPasswordAsync(
+                                user, input.Password))
+                        throw new Exception("Invalid login attempt.");
+                    else
+                    {
+                        var signingCredentials = new SigningCredentials(
+                            new SymmetricSecurityKey(
+                                System.Text.Encoding.UTF8.GetBytes(
+                                    _configuration["JWT:SigningKey"])),
+                            SecurityAlgorithms.HmacSha256);
+
+                        var claims = new List<Claim>();
+                        claims.Add(new Claim(
+                            ClaimTypes.Name, user.UserName));
+                        claims.AddRange(
+                            (await _userManager.GetRolesAsync(user))
+                                .Select(r => new Claim(ClaimTypes.Role, r)));
+
+                        var jwtObject = new JwtSecurityToken(
+                            issuer: _configuration["JWT:Issuer"],
+                            audience: _configuration["JWT:Audience"],
+                            claims: claims,
+                            expires: DateTime.Now.AddSeconds(300),
+                            signingCredentials: signingCredentials);
+
+                        var jwtString = new JwtSecurityTokenHandler()
+                            .WriteToken(jwtObject);
+
+                        return StatusCode(
+                            StatusCodes.Status200OK,
+                            jwtString);
+                    }
+                }
+                else
+                {
+                    var details = new ValidationProblemDetails(ModelState);
+                    details.Type =
+                            "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+                    details.Status = StatusCodes.Status400BadRequest;
+                    return new BadRequestObjectResult(details);
+                }
+            }
+            catch (Exception e)
+            {
+                var exceptionDetails = new ProblemDetails();
+                exceptionDetails.Detail = e.Message;
+                exceptionDetails.Status =
+                    StatusCodes.Status401Unauthorized;
+                exceptionDetails.Type =
+                        "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+                return StatusCode(
+                    StatusCodes.Status401Unauthorized,
+                    exceptionDetails);
+            }
+        }
 }
