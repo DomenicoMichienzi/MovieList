713d99f2340d135c933548a2b8c0ee7078575ac8
Dome <tecnoemod@gmail.com>
2023-07-11 15:16:19 +0200

    Added RegisterDTO class for dealing with register phase

---
+++ MovieList/DTO/RegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieList.DTO;
+
+public class RegisterDTO
+{
+    [Required]
+    [MaxLength(255)]
+    public string? UserName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(255)]
+    public string? Email { get; set; }
+
+    [Required]
+    public string? Password { get; set; }
+}
